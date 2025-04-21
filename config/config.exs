0fd9c6cf3a84e30463768cafc908de8ff1b7ca8b
ssites <ssites@liberty.edu>
2025-04-21 09:33:50 -0500

    chore: release version v0.1.0

--- config/config.exs
+++ config/config.exs
@@ -129,6 +129,36 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :git_ops,
+  mix_project: Mix.Project.get!(),
+  changelog_file: "CHANGELOG.md",
+  repository_url: "https://github.com/sethsites/sais",
+  types: [
+    # Makes an allowed commit type called `tidbit` that is not
+    # shown in the changelog
+    tidbit: [
+      hidden?: true
+    ],
+    # Makes an allowed commit type called `important` that gets
+    # a section in the changelog with the header "Important Changes"
+    important: [
+      header: "Important Changes"
+    ]
+  ],
+  tags: [
+    # Only add commits to the changelog that has the "backend" tag
+    allowed: ["backend"],
+    # Filter out or not commits that don't contain tags
+    allow_untagged?: true
+  ],
+  # Instructs the tool to manage your mix version in your `mix.exs` file
+  # See below for more information
+  manage_mix_version?: true,
+  # Instructs the tool to manage the version in your README.md
+  # Pass in `true` to use `"README.md"` or a string to customize
+  manage_readme_version: "README.md",
+  version_tag_prefix: "v"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
